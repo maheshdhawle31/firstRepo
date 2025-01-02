91bffc67d2ab38bb01229c583ae2027917549317
Vilas Kolhe <kolhe.vilas@gmail.com>
2025-01-02 10:22:33 +0530

    Test commit even or odd practice

---
+++ evenorodd.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class evenorodd {
+    public static void main(String[] args) {
+
+        Scanner Sc = new Scanner(System.in);
+
+        System.out.print(" enter a number : ");
+        int x = Sc.nextInt();
+
+        if(x % 2==0){
+            System.out.println(" it is a even number ");
+        }
+        else{
+            System.out.println(" it is a odd number ");
+        }
+
+
+
+
+
+
+    }
+
+
+}
