f6064cff52e986761d9674fafede7e8431d15da5
Vilas Kolhe <kolhe.vilas@gmail.com>
2025-01-02 10:30:50 +0530

    Test commit addition two numbers practice

--- additionoftwonumbers.java
+++ additionoftwonumbers.java
@@ -1,6 +1,6 @@
 
 import java.util.Scanner;
-public class additionoffournumbers {
+public class additionoftwonumbers {
     public static void main(String[] args) {
 
     Scanner Sc = new Scanner(System.in);
@@ -11,16 +11,11 @@ public class additionoffournumbers {
         System.out.print(" enter a second number : ");
         double second = Sc.nextDouble();
 
-        System.out.print(" enter a third number : ");
-        double third = Sc.nextDouble();
 
-        System.out.print(" enter a fourth number : ");
-        double fourth = Sc.nextDouble();
-
-        double add = first + second + third + fourth;
-        double substract = first - second - third - fourth;
-        double multiply = first * second * third * fourth;
-        double divide = first / second / third / fourth;
+        double add = first + second ;
+        double substract = first - second ;
+        double multiply = first * second ;
+        double divide = first / second ;
 
         System.out.println("the sum of = "+add);
         System.out.println(" the difference between = "+substract);
